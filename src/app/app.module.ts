0fdafc141b9167371d5a5f7d2de8aa9098eb84b3
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2023-03-14 12:54:20 +0100

    change folder structure, setup routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,15 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {LoginComponent} from './login/login.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {httpInterceptor} from "./core/service/interceptor/http.interceptor";
-import {HttpClientModule} from "@angular/common/http";
+import { AppRouting } from './app.routing';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { httpInterceptor } from './core/service/interceptor/http.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRouting, NgbModule],
   providers: [httpInterceptor],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
---
+++ src/app/modules/login/login.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from "@angular/core";
+import {LoginComponent} from "./login.component";
+import {LoginRoutingModule} from "./config/login.routing";
+
+@NgModule({
+  imports: [LoginRoutingModule],
+  declarations: [LoginComponent],
+})
+export class LoginModule {
+}
