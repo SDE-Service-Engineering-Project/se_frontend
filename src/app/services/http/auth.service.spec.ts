da67de0804e2bdc3acec55b8a58bee97e4ae0605
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2023-03-14 21:47:10 +0100

    add test for auth service; add test to toast service

--- src/app/services/http/auth.service.spec.ts
+++ src/app/services/http/auth.service.spec.ts
@@ -1,19 +1,50 @@
-import {TestBed} from '@angular/core/testing';
-import {AuthService} from "./auth.service";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { AuthService } from './auth.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
+import { HttpClient } from '@angular/common/http';
+import { AuthResponse } from '../../models/AuthResponse';
+import { mockAuthResponse } from '../../utils/testing/mocks/authResponse.mock';
 
+const AUTH_API = 'http://localhost:8080/auth';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let spectator: SpectatorService<AuthService>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
-    });
-    service = TestBed.inject(AuthService);
+  const createService = createServiceFactory({
+    service: AuthService,
+    imports: [HttpClientTestingModule],
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    spectator = createService();
+    httpClient = spectator.inject<HttpClient>(HttpClient);
+    httpTestingController = spectator.inject<HttpTestingController>(
+      HttpTestingController
+    );
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should log send an auth request to the backend', () => {
+    let authResponse: AuthResponse;
+    spectator.service.login('testUser', 'testPwd').subscribe((response) => {
+      authResponse = response;
+    });
+    const mockRequest = httpTestingController.expectOne(`${AUTH_API}/login`);
+    expect(mockRequest.request.method).toEqual('POST');
+    mockRequest.flush(mockAuthResponse);
+    expect(mockRequest.request.body).toMatchObject({
+      password: "testPwd",
+      userName: "testUser",
+    });
+    // @ts-ignore
+    expect(authResponse).toEqual(mockAuthResponse);
   });
 });
---
+++ src/app/utils/testing/mocks/authResponse.mock.ts
@@ -0,0 +1,8 @@
+import { AuthResponse } from '../../../models/AuthResponse';
+
+export const mockAuthResponse: AuthResponse = {
+  authToken: '',
+  expiresAt: Date.prototype,
+  refreshToken: '',
+  userName: 'testUser',
+};
