38042d22234dcb48a080a4ae9a81467a5884849e
Matthias Schmid-Kietreiber <55179251+mashkie@users.noreply.github.com>
2023-03-15 14:16:09 +0100

    add test for auth service; add test to toast service (#8)
    * add test for auth service; add test to toast service
    
    * fix typo in test description

--- src/app/services/toast/toast.service.spec.ts
+++ src/app/services/toast/toast.service.spec.ts
@@ -1,8 +1,11 @@
 import { ToastInfo, ToastService } from './toast.service';
-import { createServiceFactory } from '@ngneat/spectator';
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
+import { ToastComponent } from '../../shared/components/toast/toast.component';
+import { NgbToast } from '@ng-bootstrap/ng-bootstrap';
+import { getNsPrefix } from '@angular/compiler';
 
 describe('ToastService', () => {
-  let spectator: any;
+  let spectator: SpectatorService<ToastService>;
   let toast: ToastInfo = {
     text: 'this is a test toast',
     classname: 'bg-success',
@@ -10,14 +13,26 @@ describe('ToastService', () => {
     delay: 1,
   };
 
-  const createService = createServiceFactory(ToastService);
+  const createService = createServiceFactory({
+    service: ToastService,
+  });
 
   beforeEach(() => {
     spectator = createService();
   });
 
-  it('should push a specific toast to the toasts array', () => {
-    spectator.service.show(toast);
+  it('should push a toast to the toasts array', () => {
+    spectator.service.show(toast.text);
     expect(spectator.service.toasts.length).toEqual(1);
+  });
+
+  it('should remove a toast from array', () => {
+    spectator.service.show(toast.text, {
+      classname: 'bg-success',
+      body: 'this is a test body',
+      delay: 1,
+    });
+    spectator.service.remove(spectator.service.toasts[0]);
+    expect(spectator.service.toasts.length).toEqual(0);
   });
 });
