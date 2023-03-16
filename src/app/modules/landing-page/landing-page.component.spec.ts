0f856099e7aac8a8ebbaaf85648ffbb2134780e9
veidl <thomas.polzer@protonmail.com>
2023-03-16 14:31:02 +0100

    add config for gmaps for test cases

--- src/app/modules/landing-page/landing-page.component.spec.ts
+++ src/app/modules/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LandingPageComponent } from './landing-page.component';
+import {LandingPageComponent} from './landing-page.component';
+import {MapsComponent} from "../../shared/components/maps/maps.component";
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
@@ -8,7 +9,8 @@ describe('LandingPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingPageComponent ]
+      declarations: [ LandingPageComponent ],
+      providers: [MapsComponent]
     })
     .compileComponents();
 
--- src/app/shared/components/maps/maps.component.ts
+++ src/app/shared/components/maps/maps.component.ts
@@ -19,6 +19,7 @@ export class MapsComponent implements OnInit {
 
 
   ngOnInit(): void {
+
     this.getLocation()
   }
 
